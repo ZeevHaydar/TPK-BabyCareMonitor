0207703205c64518ceedef671ac35cd5028dafd7
ZeevHaydar <nzhifhaidar@gmail.com>
2024-05-14 10:19:22 +0700

    edit routing

--- baby_monitor/baby_monitor/routing.py
+++ baby_monitor/baby_monitor/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import VideoStreamConsumer
 
 websocket_urlpatterns = [
-    path('ws/video_stream/', VideoStreamConsumer.as_asgi()),
+    path('video_stream', VideoStreamConsumer.as_asgi()),
 ]
